99a31f95c90707dfc990e6bf3ab2950997a18731
JoshuaGabriel <joshuamblanch@gmail.com>
2021-09-17 00:36:35 -0700

    Initial Commit
    Working version used during club day F2021

---
+++ scripts/ROS_runt_demo/ROS_runt_demo.ino
@@ -0,0 +1,88 @@
+//#define USE_USBCON
+// rosrun rosserial_python serial_node.py /dev/ttyUSB0 _baud:=9600
+
+#include <ros.h>
+#include "runt_rover/Speed.h"
+
+// PWM pins
+#define LEFT_BACK_PIN 3
+#define LEFT_MIDDLE_PIN 5
+#define LEFT_FRONT_PIN 6
+#define RIGHT_BACK_PIN 9
+#define RIGHT_MIDDLE_PIN 10
+#define RIGHT_FRONT_PIN 11
+
+// Direction pins
+#define LEFT_BACK_DIR_PIN 2
+#define LEFT_MIDDLE_DIR_PIN 4
+#define LEFT_FRONT_DIR_PIN 7
+#define RIGHT_BACK_DIR_PIN 8
+#define RIGHT_MIDDLE_DIR_PIN 12
+#define RIGHT_FRONT_DIR_PIN 13
+
+#define FORWARD LOW
+#define REVERSE HIGH
+
+void handleSpeed(const runt_rover::Speed speed_direction){
+  
+  int write_direction_left = speed_direction.leftdirection;
+  int write_direction_right = speed_direction.rightdirection;
+  int write_speed_left = speed_direction.leftspeed;
+  int write_speed_right = speed_direction.rightspeed;
+
+    // Write motor direction
+  digitalWrite(LEFT_BACK_DIR_PIN, write_direction_left);
+  digitalWrite(LEFT_MIDDLE_DIR_PIN, write_direction_left);
+  digitalWrite(LEFT_FRONT_DIR_PIN, write_direction_left);
+  digitalWrite(RIGHT_BACK_DIR_PIN, write_direction_right);
+  digitalWrite(RIGHT_MIDDLE_DIR_PIN, write_direction_right);
+  digitalWrite(RIGHT_FRONT_DIR_PIN, write_direction_right);
+
+  // Write motor speed
+  analogWrite(LEFT_BACK_PIN, write_speed_left);
+  analogWrite(LEFT_MIDDLE_PIN, write_speed_left);
+  analogWrite(LEFT_FRONT_PIN, write_speed_left);
+  analogWrite(RIGHT_BACK_PIN, write_speed_right);
+  analogWrite(RIGHT_MIDDLE_PIN, write_speed_right);
+  analogWrite(RIGHT_FRONT_PIN, write_speed_right);
+
+    
+}
+
+
+
+  //Initalize ros node and subscriber
+
+
+ros::NodeHandle nh;
+ros::Subscriber<runt_rover::Speed> sub("data",handleSpeed);
+void setup()
+{
+  nh.getHardware()->setBaud(9600);
+  pinMode(LEFT_BACK_PIN, OUTPUT);
+  pinMode(LEFT_MIDDLE_PIN, OUTPUT);
+  pinMode(LEFT_FRONT_PIN, OUTPUT);
+  pinMode(RIGHT_BACK_PIN, OUTPUT);
+  pinMode(RIGHT_MIDDLE_PIN, OUTPUT);
+  pinMode(RIGHT_FRONT_PIN, OUTPUT);
+
+  // Configure direction pins
+  pinMode(LEFT_BACK_DIR_PIN, OUTPUT);
+  pinMode(LEFT_MIDDLE_DIR_PIN, OUTPUT);
+  pinMode(LEFT_FRONT_DIR_PIN, OUTPUT);
+  pinMode(RIGHT_BACK_DIR_PIN, OUTPUT);
+  pinMode(RIGHT_MIDDLE_DIR_PIN, OUTPUT);
+  pinMode(RIGHT_FRONT_DIR_PIN, OUTPUT);
+
+
+  nh.initNode();
+  nh.subscribe(sub);
+
+  
+}
+
+void loop()
+{
+    nh.spinOnce();
+    
+}
---
+++ scripts/joystick_demo.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python 
+ # joystick_demo.py
+# This program will read the x and y axis data from a USB connected joystick and
+# will convert that data into left/right speed values for the runt rover. Then it
+# will send those 2 values to the runt rover over serial in the form: 
+#
+# <start marker><left speed integer><seperator><right speed integer><end marker>
+#
+# The left and right speeds are values [-100, 100] where a negative value
+# specifies the reverse direction. The start marker is the '>' character. The
+# seperator is a comma. The end marker is the '<' character.
+
+import pygame
+from time import time
+import rospy
+from runt_rover.msg import Speed
+
+BAUD_RATE = 9600
+PACKAGE_SIZE = 9
+
+MAX_VAL = 100
+X_AXIS_DEADZONE = 0.14 * MAX_VAL
+Y_AXIS_DEADZONE = 0.14 * MAX_VAL
+Z_AXIS_DEADZONE = 0.20 * MAX_VAL # Rotation around z axis
+SLIDER_OFFSET = 1 * MAX_VAL # Slider in front of joystick
+
+
+
+
+class joystick_demo:
+
+    def control_runt_rover(self):
+
+        # Create an Speed Publisher joystick data
+        pub = rospy.Publisher('data',Speed,queue_size=10)
+        rospy.init_node('joystick_demo', anonymous=True)
+        rate = rospy.Rate(15)
+        
+        pygame.init()
+        current_time = time()
+
+
+        # Initialize joystick
+        try:
+            joystick = pygame.joystick.Joystick(0)
+            joystick.init()
+        except pygame.error:
+            print("Cannot find joystick. Not running joystick.")
+            return
+
+        while pygame.joystick.get_count() > 0 and not rospy.is_shutdown():
+            if (time() - current_time) < 0.05:
+                continue
+            current_time = time()
+
+            # Retrieve joystick data
+            # X and Y axis is range [-100.0, 100.0] where negative is reverse
+            pygame.event.get()
+            x_axis = joystick.get_axis(0) * MAX_VAL
+            y_axis = (0-joystick.get_axis(1))* MAX_VAL
+
+            # Rotation around Z axis is range [-100.0, 100.0] where negative is left rotation
+            z_axis = joystick.get_axis(3)* MAX_VAL
+            # print(x_axis, y_axis, z_axis)
+
+            
+            # Limiter is in range [0, 10.0] where 0 is when slider is all the way
+            # back towards negative sign
+            limiter = (0 - joystick.get_axis(2)) * MAX_VAL
+            limiter = (limiter + SLIDER_OFFSET) / 2 # Maps from [-100.0, 100.0] to [0, 100.0]
+
+            # Set initial speed before considering turning
+            speedLeft = speedRight = y_axis
+
+            if abs(x_axis) < X_AXIS_DEADZONE and abs(y_axis) < Y_AXIS_DEADZONE:
+                # Joystick is centred
+                if abs(z_axis) > Z_AXIS_DEADZONE:
+                    # Rotate rover in place
+                    speedLeft += z_axis
+                    speedRight -= z_axis
+            else:
+                # Joystick is not centred
+                if x_axis != 0:
+                    # Turn rover
+                    speedLeft += x_axis
+                    speedRight -= x_axis
+
+            # Limit values
+            speedLeft = -limiter if speedLeft < -limiter else limiter if speedLeft > limiter else speedLeft
+            speedRight = -limiter if speedRight < -limiter else limiter if speedRight > limiter else speedRight
+
+            # Arduino expects ints
+            speed_left = int(speedLeft)
+            speed_right = int(speedRight)
+
+            speed_left_setpoint = 0
+            speed_right_setpoint = 0
+
+            if(speed_left < speed_left_setpoint):
+                speed_left+=1
+            elif(speed_left>speed_left_setpoint):
+                speed_left-=1
+            
+            if(speed_right < speed_right_setpoint):
+                speed_right+=1
+            elif(speed_right>speed_right_setpoint):
+                speed_right-=1
+
+
+            write_direction_left = 0 
+            write_direction_right = 0
+            write_speed_left = 0
+            write_speed_right = 0
+            duty_cycle_max = 255
+        
+            # reverse is 0
+            # forward is 1
+
+            if(speed_left>0):
+                write_direction_left = 0
+            else:
+                write_direction_left = 1
+            
+            if(speed_right>0):
+                write_direction_right = 0
+            else:
+                write_direction_right = 1
+
+            write_speed_left = joystick_demo.remap(abs(speed_left),0,100,0,255)
+            write_speed_right = joystick_demo.remap(abs(speed_right),0,100,0,255)
+
+
+
+            msg = Speed()
+            msg.rightspeed = int(write_speed_right)
+            msg.leftspeed = int(write_speed_left)
+            msg.rightdirection = write_direction_right
+            msg.leftdirection = write_direction_left
+            # TODO: UPDATE MSG FILE FOR DIRECTION
+
+            rospy.loginfo(msg)
+            pub.publish(msg)
+            rate.sleep()
+            # rospy.loginfo(msg)
+            # pub.publish(msg)
+    
+
+    # similar to map() in arduino
+    @staticmethod
+    def remap(OldValue,OldMin,OldMax,NewMin,NewMax):
+        OldRange = OldMax - OldMin
+        if(OldRange == 0):
+            NewValue = NewMin
+        else:
+            NewRange = NewMax - NewMin
+            NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin
+        return NewValue
+        # OldRange = (OldMax - OldMin)  
+        # NewRange = (NewMax - NewMin)  
+        # NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin
+
+
+
+def main():
+    joystick_demo().control_runt_rover()
+
+
+
+    # try:
+    #     joystick_demo().control_runt_rover()
+    # except:
+    #     print("The arduino has been disconnected")
+
+if __name__ == '__main__':
+    main()
