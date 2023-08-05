23f225d8f7d7b78fbdaa04641b45008336a03eda
JoshuaGabriel <joshuamblanch@gmail.com>
2023-08-05 08:15:05 -0700

    techtoria

--- src/arm/src/joystick_arduino/arm_joystick_working.ino/arm_joystick_working.ino.ino
+++ src/arm/src/joystick_arduino/arm_joystick_working.ino/arm_joystick_working.ino.ino
@@ -21,25 +21,28 @@ int const elbowDir = 13;
 int const wristPwmy = 4;
 int const wristDiry = 5;
 
-int const wristPwmx = 6;
-int const wristDirx = 7;
+int const wristPwmx = 7;
+int const wristDirx = 6;
 
 int const gripperPwm = 8; 
-int const gripperDir1 = 52;
-int const gripperDir2 = 53;
+int const gripperDir1 = 22;
+int const gripperDir2 = 23;
+
+int gripperFlag = 0;
 
 
 void messageCb( const sensor_msgs::Joy& joystick){
 
-  frequency_shoulder = abs(map(joystick.axes[1]*10, -10, 10, -100, 100));
-  frequency_elbow = abs(map(joystick.axes[5]*10, -10, 10, -100, 100));
+//  frequency_shoulder = abs(map(joystick.axes[1]*10, -10, 10, -130, 130));
+//  frequency_elbow = abs(map(joystick.axes[5]*10, -10, 10, -100, 100));
 
-  SetPinFrequencySafe(shoulderPwm, frequency_shoulder);
-  SetPinFrequencySafe(elbowPwm, frequency_elbow);
-  SetPinFrequencySafe(wristPwmy, 1);
+//  SetPinFrequencySafe(shoulderPwm, frequency_shoulder);
+//  SetPinFrequencySafe(elbowPwm, frequency_elbow);
+  SetPinFrequencySafe(wristPwmy, 490);
+  //SetPinFrequencySafe(wristPwmx, 490);
 
-//  SetPinFrequencySafe(shoulderPwm, 100);
-//  SetPinFrequencySafe(elbowPwm, 100);
+  SetPinFrequencySafe(shoulderPwm, 120);
+  SetPinFrequencySafe(elbowPwm, 120);
 
   // light up when sending signal
   if (joystick.axes[3] > 0) {
@@ -53,34 +56,34 @@ void messageCb( const sensor_msgs::Joy& joystick){
 //  we found that sending pwm of 5 works the best for the base stepper motor
  analogWrite(basePwm,0);
  if(joystick.axes[0] > 0.5 ){
-   digitalWrite(baseDir,0);
+   digitalWrite(baseDir,1);
    analogWrite(basePwm, 5);
  }
 
  if(joystick.axes[0] < -0.5){
-   digitalWrite(baseDir,1);
+   digitalWrite(baseDir,0);
    analogWrite(basePwm, 5);
  }
  
  // SHOULDER
  pwmWrite(shoulderPwm, 0);
  if (joystick.axes[1] > 0.25) {
-    digitalWrite(shoulderDir, 0);
+    digitalWrite(shoulderDir, 1);
     pwmWrite(shoulderPwm, 155);
  }
  if (joystick.axes[1] < -0.25) {
-    digitalWrite(shoulderDir, 1);
+    digitalWrite(shoulderDir, 0);
     pwmWrite(shoulderPwm, 155);
  }
 
   // ELBOW
  pwmWrite(elbowPwm,0);
  if(joystick.axes[5] > 0.5 ){
-   digitalWrite(elbowDir,0);
+   digitalWrite(elbowDir,1);
    pwmWrite(elbowPwm, 155);
  }
  if(joystick.axes[5] < -0.5){
-   digitalWrite(elbowDir,1);
+   digitalWrite(elbowDir,0);
    pwmWrite(elbowPwm, 155);
  }
 
@@ -89,50 +92,72 @@ void messageCb( const sensor_msgs::Joy& joystick){
  if(joystick.buttons[2] == 1 ){
    digitalWrite(wristDiry,0);
    pwmWrite(wristPwmy, 20); //set pwm
+   analogWrite(gripperPwm,0); // temp solution
  }
  if(joystick.buttons[3] == 1){
    digitalWrite(wristDiry,1);
    pwmWrite(wristPwmy, 20);
+   analogWrite(gripperPwm,0);
  }
 
   // WRIST X
- analogWrite(wristPwmx,0);
- if(joystick.axes[2] > 0.5 ){
-   digitalWrite(wristDirx,0);
-   analogWrite(wristPwmx, 40);
- }
- if(joystick.axes[2] < -0.5){
+ 
+ if(joystick.axes[2] > 0.95 ){
    digitalWrite(wristDirx,1);
-   analogWrite(wristPwmx, 40);
+   analogWrite(wristPwmx, 150);
+
+ }else if(joystick.axes[2] < -0.95){
+   digitalWrite(wristDirx,0);
+   analogWrite(wristPwmx, 150);
+
+ }else{
+  analogWrite(wristPwmx,0);
  }
 
 //GRIPPER
   //squeeze 
 
- analogWrite(gripperPwm,0);
- digitalWrite(gripperDir1,LOW);
- digitalWrite(gripperDir2,LOW);
+// analogWrite(gripperPwm,0);
+// digitalWrite(gripperDir1,LOW);
+// digitalWrite(gripperDir2,LOW);
  
- if(joystick.buttons[0] == 1){
-  analogWrite(gripperPwm, 240);
-  digitalWrite(gripperDir1,HIGH);
-  digitalWrite(gripperDir2,LOW);
-  //delay(20);
-  
- }
+   if(joystick.buttons[0] == 1){
+    analogWrite(gripperPwm, 240);
+   digitalWrite(gripperDir1,HIGH);
+   digitalWrite(gripperDir2,LOW);
 
-  //un-squeeze
- if(joystick.buttons[1] == 1){
-  analogWrite(gripperPwm, 240);
+ }else if(joystick.buttons[1] == 1){
+    analogWrite(gripperPwm, 240);
   digitalWrite(gripperDir1,LOW);
   digitalWrite(gripperDir2,HIGH);
-  //delay(20);
-  
+ }else{
+   analogWrite(gripperPwm,0);
+ digitalWrite(gripperDir1,LOW);
+ digitalWrite(gripperDir2,LOW);
+ }
+
+ if(joystick.buttons[11] == 1){
+  if(gripperFlag == 1){
+    gripperFlag = 0;
+  }else if(gripperFlag == 0){
+    gripperFlag = 1;
+  }
+//  while(joystick.buttons[11] == 1){}
   
  }
 
- 
+// Serial.println(gripperFlag);
+
+//  //un-squeeze
+// if(joystick.buttons[1] == 1){
+//  analogWrite(gripperPwm, 240);
+//  digitalWrite(gripperDir1,LOW);
+//  digitalWrite(gripperDir2,HIGH);
+//  
+//  }
+// 
 }
+
 
 ros::Subscriber<sensor_msgs::Joy> sub("joy", &messageCb );
 
@@ -151,6 +176,8 @@ void setup() {
   pinMode(gripperPwm, OUTPUT);
   pinMode(gripperDir1, OUTPUT);
   pinMode(gripperDir2, OUTPUT);
+
+//  Serial.begin(57600);
   
   nh.initNode();
   nh.subscribe(sub);
--- src/arm/src/joystick_arduino/arm_joystick_working_CANtest.ino/arm_joystick_working_CANtest.ino.ino
+++ src/arm/src/joystick_arduino/arm_joystick_working_CANtest.ino/arm_joystick_working_CANtest.ino.ino
@@ -1,6 +1,13 @@
+#include <SPI.h>
+
+#include <mcp2515.h>
+
 #include <ros.h>
 #include <sensor_msgs/Joy.h>
 #include <PWM.h>
+
+struct can_frame canMsg;
+MCP2515 mcp2515(10);
 
 ros::NodeHandle  nh;
 
@@ -151,6 +158,15 @@ void setup() {
   pinMode(gripperPwm, OUTPUT);
   pinMode(gripperDir1, OUTPUT);
   pinMode(gripperDir2, OUTPUT);
+
+  Serial.begin(115200);
+  
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_125KBPS);
+  mcp2515.setNormalMode();
+  
+  Serial.println("------- CAN Read ----------");
+  Serial.println("ID  DLC   DATA");
   
   nh.initNode();
   nh.subscribe(sub);
@@ -160,4 +176,17 @@ void loop()
 {  
   nh.spinOnce();
   delay(1);
+    if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
+    Serial.print(canMsg.can_id, HEX); // print ID
+    Serial.print(" "); 
+    Serial.print(canMsg.can_dlc, HEX); // print DLC
+    Serial.print(" ");
+    
+    for (int i = 0; i<canMsg.can_dlc; i++)  {  // print the data
+      Serial.print(canMsg.data[i],HEX);
+      Serial.print(" ");
+    }
+
+    Serial.println();      
+  }
 }
