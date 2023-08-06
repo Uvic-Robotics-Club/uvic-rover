4121722684790c69e328e1036e2c9f33ec19e135
JoshuaGabriel <joshuamblanch@gmail.com>
2023-08-06 03:26:29 -0700

    working arm
    everything except base with logitech

--- src/arm/src/joystick_arduino/arm_joystick_working.ino/arm_joystick_working.ino.ino
+++ src/arm/src/joystick_arduino/arm_joystick_working.ino/arm_joystick_working.ino.ino
@@ -9,24 +9,24 @@ int32_t frequency_elbow = 10000;
 int32_t frequency_wristy = 10;
 
 //pin definitions
-int const basePwm = 2; 
-int const baseDir = 3;
+int const basePwm = 10; 
+int const baseDir = 51;
 
 int const shoulderPwm = 11;
-int const shoulderDir = 10;
+int const shoulderDir = 3;
 
 int const elbowPwm = 12;
-int const elbowDir = 13;
+int const elbowDir = 2;
 
 int const wristPwmy = 4;
-int const wristDiry = 5;
+int const wristDiry = 53;
 
-int const wristPwmx = 7;
-int const wristDirx = 6;
+int const wristPwmx = 8;
+int const wristDirx = 52;
 
-int const gripperPwm = 8; 
-int const gripperDir1 = 22;
-int const gripperDir2 = 23;
+int const gripperPwm = 13; 
+int const gripperDir1 = 6;
+int const gripperDir2 = 7;
 
 int gripperFlag = 0;
 
@@ -38,23 +38,25 @@ void messageCb( const sensor_msgs::Joy& joystick){
 
 //  SetPinFrequencySafe(shoulderPwm, frequency_shoulder);
 //  SetPinFrequencySafe(elbowPwm, frequency_elbow);
-  SetPinFrequencySafe(wristPwmy, 490);
+//  SetPinFrequencySafe(wristPwmy, 490);
   //SetPinFrequencySafe(wristPwmx, 490);
 
   SetPinFrequencySafe(shoulderPwm, 120);
   SetPinFrequencySafe(elbowPwm, 120);
 
   // light up when sending signal
-  if (joystick.axes[3] > 0) {
+  if (joystick.buttons[2] > 0) {
     digitalWrite(LED_BUILTIN, HIGH);
   } else {
     digitalWrite(LED_BUILTIN, LOW);
   }
 
+    digitalWrite(LED_BUILTIN, LOW);
 
 // BASE
 //  we found that sending pwm of 5 works the best for the base stepper motor
  analogWrite(basePwm,0);
+
  if(joystick.axes[0] > 0.5 ){
    digitalWrite(baseDir,1);
    analogWrite(basePwm, 5);
@@ -64,9 +66,11 @@ void messageCb( const sensor_msgs::Joy& joystick){
    digitalWrite(baseDir,0);
    analogWrite(basePwm, 5);
  }
+
  
  // SHOULDER
- pwmWrite(shoulderPwm, 0);
+  pwmWrite(shoulderPwm, 0);
+
  if (joystick.axes[1] > 0.25) {
     digitalWrite(shoulderDir, 1);
     pwmWrite(shoulderPwm, 155);
@@ -77,7 +81,8 @@ void messageCb( const sensor_msgs::Joy& joystick){
  }
 
   // ELBOW
- pwmWrite(elbowPwm,0);
+    pwmWrite(elbowPwm,0);
+
  if(joystick.axes[5] > 0.5 ){
    digitalWrite(elbowDir,1);
    pwmWrite(elbowPwm, 155);
@@ -87,41 +92,46 @@ void messageCb( const sensor_msgs::Joy& joystick){
    pwmWrite(elbowPwm, 155);
  }
 
+//
   // WRIST Y
- pwmWrite(wristPwmy,0);
+ analogWrite(wristPwmy,0);
  if(joystick.buttons[2] == 1 ){
    digitalWrite(wristDiry,0);
-   pwmWrite(wristPwmy, 20); //set pwm
-   analogWrite(gripperPwm,0); // temp solution
+   analogWrite(wristPwmy, 20); //set pwm
+   //analogWrite(gripperPwm,0); // temp solution
  }
- if(joystick.buttons[3] == 1){
+ if(joystick.buttons[4] == 1){
    digitalWrite(wristDiry,1);
-   pwmWrite(wristPwmy, 20);
-   analogWrite(gripperPwm,0);
+   analogWrite(wristPwmy, 20);
+   //analogWrite(gripperPwm,0);
  }
+
 
   // WRIST X
- 
- if(joystick.axes[2] > 0.95 ){
+   analogWrite(wristPwmx,0);
+ if(joystick.buttons[5] == 1 ){
    digitalWrite(wristDirx,1);
    analogWrite(wristPwmx, 150);
+   digitalWrite(LED_BUILTIN, HIGH);
 
- }else if(joystick.axes[2] < -0.95){
+
+ }else if(joystick.buttons[3] == 1){
    digitalWrite(wristDirx,0);
    analogWrite(wristPwmx, 150);
+   digitalWrite(LED_BUILTIN, HIGH);
 
- }else{
-  analogWrite(wristPwmx,0);
+
  }
+ 
 
 //GRIPPER
   //squeeze 
 
-// analogWrite(gripperPwm,0);
-// digitalWrite(gripperDir1,LOW);
-// digitalWrite(gripperDir2,LOW);
+ analogWrite(gripperPwm,0);
+ digitalWrite(gripperDir1,LOW);
+ digitalWrite(gripperDir2,LOW);
  
-   if(joystick.buttons[0] == 1){
+ if(joystick.buttons[0] == 1){
     analogWrite(gripperPwm, 240);
    digitalWrite(gripperDir1,HIGH);
    digitalWrite(gripperDir2,LOW);
@@ -130,22 +140,21 @@ void messageCb( const sensor_msgs::Joy& joystick){
     analogWrite(gripperPwm, 240);
   digitalWrite(gripperDir1,LOW);
   digitalWrite(gripperDir2,HIGH);
- }else{
-   analogWrite(gripperPwm,0);
- digitalWrite(gripperDir1,LOW);
- digitalWrite(gripperDir2,LOW);
  }
+//   analogWrite(gripperPwm,0);
+// digitalWrite(gripperDir1,LOW);
+// digitalWrite(gripperDir2,LOW);
+// 
 
- if(joystick.buttons[11] == 1){
-  if(gripperFlag == 1){
-    gripperFlag = 0;
-  }else if(gripperFlag == 0){
-    gripperFlag = 1;
-  }
+// if(joystick.buttons[11] == 1){
+//  if(gripperFlag == 1){
+//    gripperFlag = 0;
+//  }else if(gripperFlag == 0){
+//    gripperFlag = 1;
+//  }
 //  while(joystick.buttons[11] == 1){}
   
- }
-
+// }
 // Serial.println(gripperFlag);
 
 //  //un-squeeze
