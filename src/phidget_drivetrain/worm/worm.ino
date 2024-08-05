116c6714914f5a09cd1d92b7ea45df3a2f1001e6
JoshuaGabriel <joshuamblanch@gmail.com>
2024-08-05 10:46:03 -0700

    two motors working locally
    Signed-off-by: JoshuaGabriel <joshuamblanch@gmail.com>

--- src/phidget_drivetrain/worm/worm.ino
+++ src/phidget_drivetrain/worm/worm.ino
@@ -3,8 +3,11 @@
 #include <std_msgs/Float32.h>
 #include <std_msgs/String.h>
 
-Servo motor;  // Create a Servo object to control the servo motor
-const int motorPin = 9;  // Pin connected to the servo signal pin
+Servo motorleft;  // Create a Servo object to control the servo motor
+Servo motorright; 
+const int motorPinleft = 9;  
+const int motorPinright = 8; 
+
 
 ros::NodeHandle nh;
 
@@ -14,22 +17,26 @@ ros::Publisher debug_pub("debug_info", &debug_msg);
 
 void motorCommandCallback(const std_msgs::Float32& cmd_msg) {
   float command = cmd_msg.data;
-  int motorSpeed = map(command * 100, -100, 100, 0, 180);  // Map the command to motor speed
+  int motorSpeed = map(command * 100, -100, 100, 0, 180); 
+  int motorSpeedright = map(command * 100, -100, 100, 0, 180); 
 
-  // Create a debug message
+
   char buf[50];
   dtostrf(command, 4, 2, buf);  // Convert float to string
   String debugStr = "Received: " + String(buf) + ", Mapped: " + String(motorSpeed);
   debug_msg.data = debugStr.c_str();
   debug_pub.publish(&debug_msg);
 
-  motor.write(motorSpeed);
+  motorleft.write(motorSpeed);
+  motorright.write(motorSpeedright);
+
 }
 
 ros::Subscriber<std_msgs::Float32> sub("motor_command", motorCommandCallback);
 
 void setup() {
-  motor.attach(motorPin);  // Attach the servo object to the specified pin
+  motorleft.attach(motorPinleft); 
+  motorright.attach(motorPinright);
   nh.initNode();
   nh.subscribe(sub);
   nh.advertise(debug_pub);
