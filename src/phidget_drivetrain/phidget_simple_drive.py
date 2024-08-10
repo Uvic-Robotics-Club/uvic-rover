7388db7389a045718190c61a5e51cee98595b673
JoshuaGabriel <joshuamblanch@gmail.com>
2024-08-09 19:06:25 -0700

    fixed phidget back forward
    Signed-off-by: JoshuaGabriel <joshuamblanch@gmail.com>

--- src/phidget_drivetrain/phidget_simple_drive.py
+++ src/phidget_drivetrain/phidget_simple_drive.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
+
 import rospy
 from Phidget22.Phidget import *
 from Phidget22.Devices.BLDCMotor import *
 from sensor_msgs.msg import Joy
+
 
 # Return BLDC motor phidget object
 def initialize_motor(serial_number, hub_port):
@@ -12,40 +15,34 @@ def initialize_motor(serial_number, hub_port):
     motor.openWaitForAttachment(5000)
     return motor
 
+
+vint_hub_serial_number = 757580  # Replace with your VINT Hub's serial number
+
+
+motors = {
+    'back_left': initialize_motor(vint_hub_serial_number, 5),
+    'back_right': initialize_motor(vint_hub_serial_number, 0),
+    'front_left': initialize_motor(vint_hub_serial_number, 4),
+    'front_right': initialize_motor(vint_hub_serial_number, 3)
+}
+
+
 # Callback function for joy messages
 def joy_callback(data):
-    # Use the vertical axis of the left stick for forward/backward control
-    forward = data.axes[4]  # Assuming left stick vertical axis is at index 1
 
-    # Set all motors to the same velocity for forward/backward movement
-    motors['back_left'].setTargetVelocity(forward)
-    motors['back_right'].setTargetVelocity(forward)
-    motors['front_left'].setTargetVelocity(forward)
-    motors['front_right'].setTargetVelocity(forward)
+    motors['back_left'].setTargetVelocity(data.axes[4])
+    motors['back_right'].setTargetVelocity(-data.axes[4])
+    motors['front_left'].setTargetVelocity(-data.axes[4])
+    motors['front_right'].setTargetVelocity(data.axes[4])
+
 
 def main():
-    global motors
 
-    # Define the serial number of your VINT Hub
-    vint_hub_serial_number = 751007  # Replace with your VINT Hub's serial number
-
-    # Initialize motors
-    motors = {
-        'back_left': initialize_motor(vint_hub_serial_number, 0),
-        'back_right': initialize_motor(vint_hub_serial_number, 5),
-        'front_left': initialize_motor(vint_hub_serial_number, 4),
-        'front_right': initialize_motor(vint_hub_serial_number, 3)
-    }
-
-    # Initialize ROS node and subscribe to joy topic
     rospy.init_node('phidget_motor_control')
-    rospy.Subscriber('joy', Joy, joy_callback)
-
+    rate = rospy.Rate(15) 
+    rospy.Subscriber('joy', Joy, joy_callback, queue_size=2)
     rospy.spin()
 
-    # Close motors when done
-    for motor in motors.values():
-        motor.close()
 
 if __name__ == '__main__':
     main()
