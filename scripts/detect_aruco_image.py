44d8446fd4ff37f34bb24e2fe8aaa0a8a9a1929f
alicezou-ca <108016400+alicezou-ca@users.noreply.github.com>
2024-08-11 10:15:31 -0600

    Update detect_aruco_image.py

--- scripts/detect_aruco_image.py
+++ scripts/detect_aruco_image.py
@@ -7,47 +7,23 @@
 #   IMPORTS
 # -----------------------------
 # Import the necessary packages
-import argparse
 import imutils
 import cv2
-import sys
-
-# Construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to the input image containing the ArUCo tag")
-ap.add_argument("-t", "--type", type=str, default="DICT_ARUCO_ORIGINAL", help="Tpe of ArUCo tag to detect")
-args = vars(ap.parse_args())
 
 # Define the names of each possible ArUco tag that OpenCV supports
-ARUCO_DICT = {"DICT_4X4_50": cv2.aruco.DICT_4X4_50, "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
-              "DICT_4X4_250": cv2.aruco.DICT_4X4_250, "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
-              "DICT_5X5_50": cv2.aruco.DICT_5X5_50, "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
-              "DICT_5X5_250": cv2.aruco.DICT_5X5_250, "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
-              "DICT_6X6_50": cv2.aruco.DICT_6X6_50, "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
-              "DICT_6X6_250": cv2.aruco.DICT_6X6_250, "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
-              "DICT_7X7_50": cv2.aruco.DICT_7X7_50, "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
-              "DICT_7X7_250": cv2.aruco.DICT_7X7_250, "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
-              "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
-              "DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
-              "DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
-              "DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
-              "DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11}
-
+ARUCO = {"DICT": cv2.aruco.DICT_4X4_100}
+IMAGE = {"PATH": "file/image.png"}
 # Load the input image from disk and resize it
-print("[INFO] Loading image...")
-image = cv2.imread(args["image"])
+#print("[INFO] Loading image...")
+image = cv2.imread(IMAGE["PATH"])
 image = imutils.resize(image, width=600)
 
-# Verify that the supplied ArUCo tag exists is supported by OpenCV
-if ARUCO_DICT.get(args["type"], None) is None:
-    print("[INFO] ArUCo tag of '{}' is not supported!".format(args["type"]))
-    sys.exit(0)
-
 # Load the ArUCo dictionary, grab the ArUCo parameters and detect the markers
-print("[INFO] Detecting '{}' tags...".format(args["type"]))
-arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[args["type"]])
-arucoParams = cv2.aruco.DetectorParameters_create()
+#print("[INFO] Detecting '{}' tags...".format(ARUCO["DICT"]))
+arucoDict = cv2.aruco.getPredefinedDictionary(ARUCO["DICT"])
+arucoParams = cv2.aruco.DetectorParameters()
 (corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict, parameters=arucoParams)
+# print(ids)
 
 # Verify *at least* one ArUCo marker was detected
 if len(corners) > 0:
@@ -78,5 +54,4 @@ if len(corners) > 0:
         print("[INFO] ArUco marker ID: {}".format(markerID))
         # Show the output image
         cv2.imshow("Image", image)
-        cv2.waitKey(0)
 
