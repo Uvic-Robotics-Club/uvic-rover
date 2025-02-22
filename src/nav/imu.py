1a492a660e71af1bdf1f3d785ea6f21cb5df6b96
Isaiah Driver <isaiahdriver1653@gmail.com>
2025-02-22 05:42:38 -0800

    Added EKF yaml and ROS robot_localization launch files, along with additional functionality for imu.py so as to provide orientation data (and other misc. changes).

--- src/nav/imu.py
+++ src/nav/imu.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
-
+import math
 import serial
 import rospy
 from sensor_msgs.msg import Imu
-
-def get_parsed_serial_data(raw_serial_data):
-    listed_ser_data = raw_serial_data.split()
-    parsed_ser_data = []
-    for x in listed_ser_data:
-        parsed_ser_data.append(float(x))  
-    return parsed_ser_data 
+import tf.transformations as tft
 
 def shutdown_hook(ser_port):
     rospy.loginfo("Shutting down IMU node...")
@@ -17,39 +11,56 @@ def shutdown_hook(ser_port):
 
 def main():
     rospy.init_node('imu_talker')
-    imu_pub = rospy.Publisher('IMU', Imu, queue_size=10)    
+    imu_pub = rospy.Publisher('imu', Imu, queue_size=10)    
     imu_msg = Imu()
-    #rate = rospy.Rate(0.1)  
+    rate = rospy.Rate(30)  
+    
+    ser_port = serial.Serial('/dev/ttyACM0', 115200, timeout=0.1)   # 1/timeout is the frequency at which the port is read
+    ser_port.reset_input_buffer() # (serial port may still contain some stray bytes)
 
-    ser_port = serial.Serial('/dev/ttyACM1', 115200, timeout=0.1)   # 1/timeout is the frequency at which the port is read
     rospy.on_shutdown(lambda: shutdown_hook(ser_port))
 
     while not rospy.is_shutdown():
         try:
-            raw_serial_data = ser_port.readline().decode().strip()
-            if raw_serial_data:
-                imu_data = get_parsed_serial_data(raw_serial_data)
-                if len(imu_data) == 9:
+            raw_ser = ser_port.readline().decode().strip()
+            if raw_ser:
+                parsed_ser = [float(x) for x in raw_ser.split()]
+                if len(parsed_ser) == 9:
+                    # Compute roll and pitch from accelerometer data:
+                    roll = math.atan2(parsed_ser[1], parsed_ser[2])
+                    pitch = math.atan2(-parsed_ser[0], math.sqrt(parsed_ser[1]**2 + parsed_ser[2]**2))
+
+                    # Compensate magnetometer readings to compute yaw:
+                    mag_x, mag_y, mag_z = parsed_ser[3], parsed_ser[4], parsed_ser[5]
+                    mag_x_comp = mag_x * math.cos(pitch) + mag_z * math.sin(pitch)
+                    mag_y_comp = mag_x * math.sin(roll) * math.sin(pitch) + mag_y * math.cos(roll) - mag_z * math.sin(roll) * math.cos(pitch)
+                    yaw = math.atan2(-mag_y_comp, mag_x_comp)
+
+                    # Convert to quaternion:
+                    quat = tft.quaternion_from_euler(roll, pitch, yaw)
 
                     imu_msg.header.stamp = rospy.Time.now()
                     imu_msg.header.frame_id = "imu_frame"
-
-                    imu_msg.linear_acceleration.x = imu_data[0] #linear acceleration x
-                    imu_msg.linear_acceleration.y = imu_data[1] #linear acceleration y
-                    imu_msg.linear_acceleration.z = imu_data[2] #linear acceleration z
-                    imu_msg.angular_velocity.x = imu_data[6] #angular velocity x
-                    imu_msg.angular_velocity.y = imu_data[7] #angular velocity y
-                    imu_msg.angular_velocity.z = imu_data[8] #angular velocity z
+                    imu_msg.orientation.x = quat[0]
+                    imu_msg.orientation.y = quat[1]
+                    imu_msg.orientation.z = quat[2]
+                    imu_msg.orientation.w = quat[3]
+                    imu_msg.linear_acceleration.x = parsed_ser[0] #linear acceleration x
+                    imu_msg.linear_acceleration.y = parsed_ser[1] #linear acceleration y
+                    imu_msg.linear_acceleration.z = parsed_ser[2] #linear acceleration z
+                    imu_msg.angular_velocity.x = parsed_ser[6] #angular velocity x
+                    imu_msg.angular_velocity.y = parsed_ser[7] #angular velocity y
+                    imu_msg.angular_velocity.z = parsed_ser[8] #angular velocity z
+                    imu_pub.publish(imu_msg)
 
                     #Magnetometer paremeters are not included by default in the IMU message format used here
+ 
+                    rospy.loginfo("linear accel - x: %f, y: %f, z: %f", parsed_ser[0], parsed_ser[1], parsed_ser[2])
+                    rospy.loginfo("mag - x:%f, y:%f, z:%f", parsed_ser[3], parsed_ser[4], parsed_ser[5])
+                    rospy.loginfo("angular vel - x:%f, y:%f, z:%f", parsed_ser[6], parsed_ser[7], parsed_ser[8])
+                    rospy.loginfo("orientation - x:%f, y:%f, z:%f", quat[0], quat[1], quat[2])
 
-                    imu_pub.publish(imu_msg)
-        
-                    print("Accel:", imu_data[0], imu_data[1], imu_data[2])
-                    print("Mag:", imu_data[3], imu_data[4], imu_data[5])
-                    print("Gyro:", imu_data[6], imu_data[7], imu_data[8], "\n")
-
-            #rate.sleep()
+            rate.sleep()
 
         except serial.SerialException as e:
             rospy.logerr(f"Serial port error: {e}")
